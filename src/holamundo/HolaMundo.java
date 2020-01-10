c1cfd4256d439755d30ca758ebea9fafe6d3b301
rcarreraamoedo <rcarreraamoedo@danielcastelao.org>
2020-01-10 14:17:16 +0100

    Otra prueba

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -17,6 +17,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("HOLA MUNDO!!");
         System.out.println("ADIÓS");
+        System.out.println("ExpermentoColores");
     }
     
 }
